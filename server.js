b48c383133a629977b185ffc626d8451ce7df5c4
User <splimis@gmail.com>
2017-10-12 21:48:47 -0400

    maybe not6

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-
-var port = process.env.PORT || 3000;
+var exphbs = require('express-handlebars');
 
 var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+
+
 
 // Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
 // Set Handlebars as the view engine
-var exphbs = require('express-handlebars');
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
